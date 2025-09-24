9c7e9fe828b31f0edb11f33cbc6e0cf66b2168a5
Andrés Sánchez <cloud@DESKTOP-1TQN06K.localdomain>
2025-09-23 22:42:35 -0600

    [Development] Base design completed. About and Contact prototype pages completed. Base web program complete.

--- atm/urls.py
+++ atm/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, AboutView, ContactView
 
 app_name = "atm"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    # path("transaction/", views.IndexView.as_view(), name="atm"),
+    path("about/", AboutView.as_view(), name="about"),
+    path("contact/", ContactView.as_view(), name="contact"),
 ]
--- atm/views.py
+++ atm/views.py
@@ -6,4 +6,9 @@ from accounts.models import UserATM
 
 class HomeView(generic.TemplateView):
     template_name = "atm/home.html"
-    
+
+class AboutView(generic.TemplateView):
+    template_name = "site/about.html"
+
+class ContactView(generic.TemplateView):
+    template_name = "site/contact.html"
